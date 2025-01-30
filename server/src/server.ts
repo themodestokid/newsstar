6c3ce98316bd32f5eb2a2f2c74770e35290a0db1
jpoor33 <74677747+jpoor33@users.noreply.github.com>
2025-01-30 14:23:29 -0500

    updating server code

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,3 +1,19 @@
-import dotenv from 'dotenv';
-dotenv.config();
-console.log(process.env.DB_URL);
+const forceDatabaseRefresh = false; 
+
+import express from 'express';
+import sequelize from './config/connection.js'; // Import the initialized Sequelize instance
+// import routes from './routes/index.js';  // Import the routes for handling different endpoints
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app. use(express.static('../client/dist'));
+
+app.use(express.json());
+// app.use(routes);
+
+sequelize.sync({ force: forceDatabaseRefresh}).then(() => {
+    app.listen(PORT, () => {
+        console.log(`Server is listening on port ${PORT}`)
+    })
+})
