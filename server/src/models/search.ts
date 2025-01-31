b2227ee96372651c64b135556c526cc6a6fae920
jpoor33 <74677747+jpoor33@users.noreply.github.com>
2025-01-31 14:14:46 -0500

    resolving merge conflicts

--- server/src/models/search.ts
+++ server/src/models/search.ts
@@ -1,28 +1,27 @@
-// import { DataTypes, Model, Optional, Sequelize } from "sequelize";
+import { DataTypes, Model, Optional, Sequelize } from "sequelize";
 
-// interface SearchAttributes {
-//     id: number;
-//     user_id: number;
-//     searchterm: string;
-//     sortBy: string;
-//     sources: string;
-//     from: Date;
-//     to: Date;
-// }
+interface SearchAttributes {
+    id: number;
+    user_id: number;
+    searchterm: string;
+    sortBy: string;
+    sources: string;
+    from: Date;
+    to: Date;
+}
 
-// interface SearchCreationAtrributes extends Optional<SearchAttributes, 'id'> { }
+interface SearchCreationAtrributes extends Optional<SearchAttributes, 'id'> { }
 
-// export class Search extends Model<SearchAttributes, SearchCreationAtrributes> implements SearchAttributes{
-//     public id!: number;
-//     public user_id!: number;
-//     public searchterm!: string;
-//     public sortBy!: string;
-//     public sources!: string;
-//     public from!: Date;
-//     public to!: Date;
-// }
+export class Search extends Model<SearchAttributes, SearchCreationAtrributes> implements SearchAttributes{
+    public id!: number;
+    public user_id!: number;
+    public searchterm!: string;
+    public sortBy!: string;
+    public sources!: string;
+    public from!: Date;
+    public to!: Date;
+}
 
-<<<<<<< Updated upstream
 export function SearchFactory(sequelize: Sequelize): typeof Search {
     Search.init(
         {
@@ -60,45 +59,6 @@ export function SearchFactory(sequelize: Sequelize): typeof Search {
             sequelize,
         }
     );
-=======
-// export function SearchFactory(sequelize: Sequelize): typeof Search {
-//     Search.init(
-//         {
-//             id: {
-//                 type: DataTypes.INTEGER,
-//                 autoIncrement: true,
-//                 primaryKey: true
-//             },
-//             user_id: {
-//                 type: DataTypes.INTEGER,
-//                 allowNull: false
-//             },
-//             searchterm: {
-//                 type: DataTypes.STRING,
-//                 allowNull: false,
-//             },
-//             sortBy: {
-//                 type: DataTypes.STRING,
-//                 allowNull: true,
-//             },
-//             sources: {
-//                 type: DataTypes.STRING,
-//                 allowNull: true,
-//             },
-//             from: {
-//                 type: DataTypes.DATE,
-//                 allowNull: true,
-//             }
-//             to: {
-//                 type: DataTypes.
-//             }
-//         ,}
-//         {
-//             tableName: 'search',
-//             sequelize,
-//         }
-//     );
->>>>>>> Stashed changes
 
-//     return Search;
-// }
+    return Search;
+}
--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -1,19 +1,12 @@
 import { Router } from 'express';
 const router = Router();
 
-<<<<<<< Updated upstream
-import apiRoutes from './api/index.js';
-import htmlRoutes from './htmlRoutes.js';
-
-router.use('/api', apiRoutes);
-=======
 import newsRoutes from './api/newsRoutes.js';
 import searchRoutes from './api/searchRoutes.js'
 import htmlRoutes from './htmlRoutes.js';
 
 router.use('/api', newsRoutes);
 router.use('/api', searchRoutes);
->>>>>>> Stashed changes
 router.use('/', htmlRoutes);
 
 
