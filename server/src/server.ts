ec6270e0bd554bd6f8c96951f60789cf5d90916a
jpoor33 <74677747+jpoor33@users.noreply.github.com>
2025-01-31 11:58:40 -0500

    removed comments from server file

--- server/src/server.ts
+++ server/src/server.ts
@@ -2,7 +2,7 @@ const forceDatabaseRefresh = false;
 
 import express from 'express';
 import sequelize from './config/connection.js'; // Import the initialized Sequelize instance
-// import routes from './routes/index.js';  // Import the routes for handling different endpoints
+import routes from './routes/index.js';  // Import the routes for handling different endpoints
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -10,7 +10,7 @@ const PORT = process.env.PORT || 3001;
 app. use(express.static('../client/dist'));
 
 app.use(express.json());
-// app.use(routes);
+app.use(routes);
 
 sequelize.sync({ force: forceDatabaseRefresh}).then(() => {
     app.listen(PORT, () => {
